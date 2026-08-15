de89fd412baf6fab0dbb9247ef0c87aee62776b7
James NeeDell <jamesneedell3@gmail.com>
2026-08-15 14:01:18 -0600

    noted instructions on how to commit and push to Git

---
+++ exploration.py
@@ -0,0 +1,5 @@
+#%%
+# commiting with git
+# git add exploration.py            # tells Git to include this files changes in my next staging (checkpoint)
+# git commit -m "noted instructions on how to push and commit for git" # git creates this staging only on mac
+# git push # checkpoint is uploaded to github and visible to anyone who looks at remote repository (repo) aka the project folder
