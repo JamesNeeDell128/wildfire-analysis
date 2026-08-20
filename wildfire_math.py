78535a425ae6df0e1c22a5e88f28a0a4df771c57
James NeeDell <jamesneedell3@gmail.com>
2026-08-20 11:57:31 -0600

    Idea to use beta distribution for an app that predicts the probability of a given day of the year having a high medium or low probability for fire to start

--- wildfire_math.py
+++ wildfire_math.py
@@ -35,3 +35,42 @@ dz_dx_no_chain = diff(z.subs(y, _y))
 print(dz_dx_chain) # take the deriv of each, multiply then sub
 print(dz_dx_no_chain) # sub then take deriv
 # %%
+# could use beta distribution to create app that tells people the 
+# probability on a given day that the chances of a fire are high med low etc
+# note: high, med, low is arbitrary for now
+
+# note: could include fire size. Ask what is the probability that the 
+# probability of a big fire will be high on a given day?
+
+
+# use to calculate the probability that probability is a certain value given a 
+# number of successes and failures out of n trials
+
+
+from scipy.stats import beta
+
+a = 30 # number of times where fire starts on given day
+b = 6 # number of times where fire does not start on a given day
+
+# use cumulative density function to calcultate area under curve which is probability
+
+
+# tells us that given x_fire_start_days and x_non_fire_start 
+# days on a given day of the year what is the probability that fire danger (or fire probability) will 
+# be high on that day of the year
+high_p = 1 - beta.cdf(0.80, a, b) 
+print(high_p) 
+
+# tells us that given x_fire_start_days and x_non_fire_start 
+# days on a given day of the year what is the probability that fire danger (or fire probability) will 
+# be medium on that day of the year
+med_p = beta.cdf(0.79, a, b) - beta.cdf(0.3, a, b)
+print(med_p) 
+
+# tells us that given x_fire_start_days and x_non_fire_start 
+# days on a given day of the year what is the probability that fire danger (or fire probability) will 
+# be low on that day of the year
+low_p = beta.cdf(0.29, a, b) 
+print(low_p) 
+
+# %%
