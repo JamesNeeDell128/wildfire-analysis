d574a5e46e033f6a65dd8ccf43f09f2225a96b46
James NeeDell <jamesneedell3@gmail.com>
2026-08-21 09:30:00 -0600

    note on Jefreys prior and arcsine transform and certainty reporting

--- wildfire_app.py
+++ wildfire_app.py
@@ -34,17 +34,35 @@ b = 6 # number of times where fire does not start on a given day
 # tells us that given x_fire_start_days and x_non_fire_start 
 # days on a given day of the year what is the probability that fire danger (or fire probability) will 
 # be high on that day of the year
-high_p = 1 - beta.cdf(0.80, a, b) 
+high_p = 1 - beta.cdf(0.80, a, b) # re-examine buckets if using Jefreys
 print(high_p) 
 
 # tells us that given x_fire_start_days and x_non_fire_start 
 # days on a given day of the year what is the probability that fire danger (or fire probability) will 
 # be medium on that day of the year
-med_p = beta.cdf(0.79, a, b) - beta.cdf(0.3, a, b)
+med_p = beta.cdf(0.8, a, b) - beta.cdf(0.3, a, b)
 print(med_p) 
 
 # tells us that given x_fire_start_days and x_non_fire_start 
 # days on a given day of the year what is the probability that fire danger (or fire probability) will 
 # be low on that day of the year
-low_p = beta.cdf(0.29, a, b) 
+low_p = beta.cdf(0.3, a, b) 
 print(low_p) 
+
+#%%
+# can use variance and mean behind the scenes to esitmate how confident we are in the prediction of the probabilities
+# for each day will have a variance. mean will tell me central estimate of fire probability on that day
+# var for beta  = a*b / ((a + b)**2 * (a + b + 1)) # adjust parameters Jefrey's prior
+
+# use arcsine transformation to normalize boundary squeeze if comparing across days
+# because days will have the same sample size
+# or just report a + b for confidence alongside the mean
+
+
+# mean
+# sum of all values / count = (30 * 1 + 6 * 0) / 36 = a / (a + b) = mean
+
+
+# also : use Jeffrey's prior here to protect against perfect certainty if a or b = 0
+# adjust parameters a' = a + 1/2, b' = b + 1/2
+# the 1/2 assumes 1/2 success, 1/2 failure before data arrives
